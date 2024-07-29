3d69fb7a4d091e3c81bf9645f66a462a77413bc8
sothulthorn <thornsothul@gmail.com>
2024-07-29 21:36:43 +0900

    add individual event card

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -9,12 +9,11 @@ type EventsPageProps = {
 };
 
 const EventsPage = async ({ params }: EventsPageProps) => {
+  const city = params.city;
   const response = await fetch(
-    'https://bytegrad.com/course-assets/projects/evento/api/events?city=austin'
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
   const events: EventsTrackerEvent[] = await response.json();
-
-  const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
